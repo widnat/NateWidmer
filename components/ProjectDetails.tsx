71ddc59fdaac83fa3309bd9a51fbb255f8da1668
Nate Widmer <widnat@gmail.com>
2023-02-12 00:13:24 -0700

    Learning css

---
+++ components/ProjectDetails.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useRouter } from 'next/router';
+import styles from '@/styles/Home.module.css'
+import { Inter } from '@next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
+type Props = {
+    route: string, 
+    title: string, 
+    description: string 
+}
+
+export default function ProductDetails({route, title, description} : Props) {
+    const router = useRouter()
+    
+    function navigate() {
+        router.push(route);
+    }
+
+    return (
+        <a className={styles.card}
+           target="_blank"
+           rel="noopener noreferrer"
+           onClick={navigate} >
+            <h2>
+                { title }
+            </h2>
+            <p>
+                {description}
+            </p>
+        </a>
+    )
+}
