aa74f97fa5d0fa89ab9da3bfc2cfe37c93477f7c
Nate Widmer <widnat@gmail.com>
2023-02-19 18:39:59 -0700

    Show/Hide Menu

--- components/NavBar/MenuBtn.tsx
+++ components/NavBar/MenuBtn.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Bool } from "reselect/es/types";
 
-export default function MenuBtn() {
+type Props = {
+	changeDropdown: any;
+};
+
+export default function MenuBtn({ changeDropdown }: Props) {
 	return (
 		<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 			<button
@@ -8,11 +13,12 @@ export default function MenuBtn() {
 				className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 				aria-controls="mobile-menu"
 				aria-expanded="false"
+				onClick={() => changeDropdown()}
 			>
 				<span className="sr-only">Open main menu</span>
 				{/* Icon when menu is closed.
                               Menu open: "hidden", Menu closed: "block"
-                        */}
+                */}
 				<svg
 					className="block h-6 w-6"
 					fill="none"
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import HrefBtn from "./HrefBtn";
 import MobileHrefBtn from "./MobileHrefBtn";
 import MenuBtn from "./MenuBtn";
+import { useState } from "react";
 
 export default function NavBar() {
+	const [dropdownIsShowing, setDropdownIsShowing] = useState<Boolean>(false);
+
+	function changeDropdown() {
+		setDropdownIsShowing(!dropdownIsShowing);
+	}
+
 	return (
 		<nav className="bg-gray-800">
 			<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 				<div className="relative flex h-16 items-center justify-between">
-					<MenuBtn />
+					<MenuBtn changeDropdown={changeDropdown} />
 					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 						<div className="hidden sm:ml-6 sm:block">
 							<div className="flex space-x-4">
@@ -25,15 +32,16 @@ export default function NavBar() {
 				</div>
 			</div>
 
-			{/* Mobile menu, show/hide based on menu state. */}
-			<div className="sm:hidden" id="mobile-menu">
-				<div className="space-y-1 px-2 pt-2 pb-3">
-					<MobileHrefBtn route="#" text="Home" />
-					<MobileHrefBtn route="#" text="Memoizit" />
-					<MobileHrefBtn route="#" text="skull King Keeper" />
-					<MobileHrefBtn route="#" text="Calculator" />
+			{dropdownIsShowing && (
+				<div className="sm:hidden" id="mobile-menu">
+					<div className="space-y-1 px-2 pt-2 pb-3">
+						<MobileHrefBtn route="#" text="Home" />
+						<MobileHrefBtn route="#" text="Memoizit" />
+						<MobileHrefBtn route="#" text="skull King Keeper" />
+						<MobileHrefBtn route="#" text="Calculator" />
+					</div>
 				</div>
-			</div>
+			)}
 		</nav>
 	);
 }
