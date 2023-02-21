7ad72d6ff4fa9c77bfcda9685a852b40cfe3146a
Nate Widmer <widnat@gmail.com>
2023-02-20 23:45:15 -0700

    Calculator done

--- pages/calculator/calculator.tsx
+++ pages/calculator/calculator.tsx
@@ -5,78 +5,92 @@ import DarkBtn from "../../components/calculator/DarkBtn";
 import NavBar from "../../components/NavBar/NavBar";
 
 export default function Calculator() {
-	const [inputNum, setInputNum] = useState("");
 	const [firstNum, setFirstNum] = useState("");
-	const [secondNum, setsecondNum] = useState("");
+	const [secondNum, setSecondNum] = useState("");
 	const [total, setTotal] = useState("");
 	const [operator, setOperator] = useState("");
 
 	function clear() {
-		alert("clear");
-		setInputNum("");
 		setTotal("");
 		setFirstNum("");
-		setsecondNum("");
+		setSecondNum("");
 		setOperator("");
 	}
 
+	function backSpace() {
+		var num1: string = firstNum;
+		var num2: string = secondNum;
+		if (secondNum !== "")
+			setSecondNum((num2 = secondNum.substring(0, secondNum.length - 1)));
+		else if (operator !== "") setOperator("");
+		else if (firstNum !== "")
+			setFirstNum((num1 = firstNum.substring(0, firstNum.length - 1)));
+
+		if (num1 === "") setTotal("");
+
+		updateTotal(num1, num2);
+	}
+
 	function appendNum(num: string) {
-		setInputNum(inputNum + num);
+		var num1: string = firstNum;
+		var num2: string = secondNum;
+		if (operator === "") num1 = firstNum + num;
+		else num2 = secondNum + num;
+
+		setFirstNum(num1);
+		setSecondNum(num2);
+		updateTotal(num1, num2);
 	}
 
 	function handleOperator(op: string) {
+		var num1: string = firstNum;
+		var num2: string = secondNum;
 		if (secondNum !== "") {
-			if (operator === "+/-") changeValueHelper((num: number) => num * -1);
-			else if (operator === "%") changeValueHelper((num: number) => num / 100);
-			else if (operator === "/")
-				doMath((num1: number, num2: number) => num1 / num2, op);
-			else if (operator === "x")
-				doMath((num1: number, num2: number) => num1 * num2, op);
-			else if (operator === "-")
-				doMath((num1: number, num2: number) => num1 - num2, op);
-			else if (operator === "+")
-				doMath((num1: number, num2: number) => num1 + num2, op);
-
-			if (op === "/" || op === "x" || op === "-" || op === "+") setOperator(op);
+			if (op === "+/-") num2 = String(Number(secondNum) * -1);
+			else if (op === "%") num2 = String(Number(secondNum) / 100);
+		} else if (firstNum !== "") {
+			if (op === "+/-") num1 = String(Number(firstNum) * -1);
+			else if (op === "%") num1 = String(Number(firstNum) / 100);
+		} else if (total !== "") {
+			setFirstNum((num1 = total));
 		}
 
-		if (firstNum !== "") {
-			if (op === "+/-") changeValueHelper((num: number) => num * -1);
-			else if (op === "%") changeValueHelper((num: number) => num / 100);
-			else if (op === "/")
-				mathOpHelper((num1: number, num2: number) => num1 / num2);
-			else if (op === "x")
-				mathOpHelper((num1: number, num2: number) => num1 * num2);
-			else if (op === "-")
-				mathOpHelper((num1: number, num2: number) => num1 - num2);
-			else if (op === "+")
-				mathOpHelper((num1: number, num2: number) => num1 + num2);
-			else if (op === "=") {
-				if (secondNum === "") {
-					setOperator("");
-					setTotal(firstNum);
-				} else
-					mathOpHelper((num1: number, num2: number) => num1 + num2, operator);
+		update(num1, num2, op);
+	}
+
+	function update(num1: string, num2: string, op: string) {
+		updateTotal(num1, num2);
+		if (op === "/" || op === "x" || op === "-" || op === "+") {
+			if (num2 !== "") {
+				setFirstNum(combineNumsWithOp(num1, num2));
+				setSecondNum("");
 			}
-		}
-	}
 
-	function changeValueHelper(opFunction: any) {
-		if (secondNum === "") {
-			let num: number = opFunction(Number(firstNum));
-			setFirstNum(String(num));
+			setOperator(op);
+		} else if (op === "=") {
+			setFirstNum("");
+			setSecondNum("");
+			setOperator("");
 		} else {
-			let num: number = opFunction(Number(secondNum));
-			setFirstNum(String(num));
+			setFirstNum(num1);
+			setSecondNum(num2);
+			if (op !== "+/-" && op !== "%") setOperator("");
 		}
 	}
 
-	function doMath(opFunction: any, op: string) {
-		let num: number = opFunction(Number(firstNum), Number(secondNum));
-		setFirstNum(String(num));
-		setsecondNum("");
-		setTotal(String(num));
-		setOperator(op);
+	function updateTotal(num1: string, num2: string) {
+		if (num2 !== "") setTotal(combineNumsWithOp(num1, num2));
+		else if (num1 !== "") setTotal(num1);
+	}
+
+	function combineNumsWithOp(num1: string, num2: string) {
+		var result: string = "";
+		if (operator === "/") result = String(Number(num1) / Number(num2));
+		else if (operator === "x") result = String(Number(num1) * Number(num2));
+		else if (operator === "-") result = String(Number(num1) - Number(num2));
+		else if (operator === "+") result = String(Number(num1) + Number(num2));
+
+		return result;
 	}
 
 	return (
@@ -88,10 +102,12 @@ export default function Calculator() {
 						<div className="p-5 text-white text-center text-3xl">
 							Calculator
 						</div>
-						<div className="pt-16 p-5 pb-0 text-white text-right text-3xl">
+						<div className=" p-5 pb-0 bg-slate-800 text-white text-right text-3xl">
 							{firstNum} {operator} {secondNum}
 						</div>
-						<div className="p-5 text-white text-right text-3xl">= {total}</div>
+						<div className="p-5 text-white bg-slate-800 text-right text-3xl">
+							= {total}
+						</div>
 
 						<div className="flex items-stretch h-24">
 							<GrayBtn action={clear} text="C" />
@@ -122,7 +138,7 @@ export default function Calculator() {
 						</div>
 
 						<div className="flex items-stretch h-24 mb-4">
-							<DarkBtn action={appendNum} text="0" />
+							<DarkBtn action={backSpace} text="<--" />
 							<DarkBtn action={appendNum} text="0" />
 							<DarkBtn action={appendNum} text="." />
 							<ColorBtn action={handleOperator} text="=" />
