69e9658e7c60cdcca83c2f74077ca1b681e84dae
Nate Widmer <widnat@gmail.com>
2023-03-10 01:06:20 -0700

    Added colors and line width slider

--- components/doodler/DrawingArea.tsx
+++ components/doodler/DrawingArea.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import { FC, SetStateAction, useState } from "react";
-import { ChromePicker } from "react-color";
+import {
+	FC,
+	SetStateAction,
+	useState,
+	useRef,
+	useEffect,
+	useLayoutEffect,
+} from "react";
 import { Draw } from "@/types/doodler";
 import { useDraw } from "@/hooks/doodler";
 
 export default function DrawingArea() {
-	const [color, setColor] = useState<string>("#000");
+	const [color, setColor] = useState<string>("black");
 	const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
+	const [lineWidth, setLineWidth] = useState(5);
+	const [canvasWidth, setCanvasWidth] = useState(50);
+	const [canvasHeight, setCanvasHeight] = useState(50);
 
 	function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
 		const { x: currX, y: currY } = currentPoint;
 		const lineColor = color;
-		const lineWidth = 5;
 
 		let startPoint = prevPoint ?? currentPoint;
 		ctx.beginPath();
@@ -28,28 +36,89 @@ export default function DrawingArea() {
 		ctx.fill();
 	}
 
+	function handleResize() {
+		setCanvasWidth(window.innerWidth / 2);
+		setCanvasHeight(window.innerWidth / 2);
+	}
+
+	useEffect(() => {
+		handleResize();
+	}, []);
+
 	return (
-		<div className="w-screen h-screen bg-white flex justify-center items-center">
-			<div className="flex flex-col gap-10 pr-10">
-				<ChromePicker
-					color={color}
-					onChange={(e: { hex: SetStateAction<string> }) => setColor(e.hex)}
+		<div className="flex items-center justify-center">
+			<div className="flex-col">
+				<canvas
+					ref={canvasRef}
+					onMouseDown={onMouseDown}
+					width={canvasWidth}
+					height={canvasHeight}
+					className="border border-teal-300 rounded-md"
 				/>
-				<button
-					type="button"
-					className="p-2 rounded-md border border-black"
-					onClick={clear}
-				>
-					Clear canvas
-				</button>
+				<div className="flex-row">
+					<div className="flex self-stretch justify-center my-5">
+						<input
+							onChange={(e) => setLineWidth(e.target.valueAsNumber)}
+							value={lineWidth}
+							className="w-64 mr-10 cursor-pointer accent-teal-500 rounded-full"
+							type="range"
+							min="4"
+							max="20"
+							step="1"
+						/>
+						<div
+							onClick={() => alert("hey")}
+							className="font-bold text-gray-700 rounded-full bg-gray-300 flex-col items-center justify-center font-mono h-6 w-6"
+						/>
+					</div>
+					<div className="flex self-stretch justify-center my-5">
+						<div
+							onClick={() => alert("hey")}
+							className="font-bold mx-3 text-gray-700 rounded-full bg-red-500 flex-col items-center justify-center font-mono h-8 w-8"
+						/>
+						<div
+							onClick={() => alert("hey")}
+							className="font-bold mx-3 text-gray-700 rounded-full bg-orange-400 flex-col items-center justify-center font-mono h-8 w-8"
+						/>
+						<div
+							onClick={() => alert("hey")}
+							className="font-bold mx-3 text-gray-700 rounded-full bg-yellow-300 flex-col items-center justify-center font-mono h-8 w-8"
+						/>
+						<div
+							onClick={() => alert("hey")}
+							className="font-bold mx-3 text-gray-700 rounded-full bg-green-400 flex-col items-center justify-center font-mono h-8 w-8"
+						/>
+						<div
+							onClick={() => alert("hey")}
+							className="font-bold mx-3 text-gray-700 rounded-full bg-blue-400 flex-col items-center justify-center font-mono h-8 w-8"
+						/>
+						<div
+							onClick={() => alert("hey")}
+							className="font-bold mx-3 text-gray-700 rounded-full bg-orange-800 flex-col items-center justify-center font-mono h-8 w-8"
+						/>
+						<div
+							onClick={() => alert("hey")}
+							className="font-bold mx-3 text-gray-700 rounded-full bg-black flex-col items-center justify-center font-mono h-8 w-8"
+						/>
+					</div>
+					<div className="flex self-stretch justify-center">
+						<button
+							type="button"
+							className="p-2 mr-10 rounded-md border border-black"
+							onClick={clear}
+						>
+							Submit Drawing
+						</button>
+						<button
+							type="button"
+							className="p-2 rounded-md border border-black"
+							onClick={clear}
+						>
+							Clear canvas
+						</button>
+					</div>
+				</div>
 			</div>
-			<canvas
-				ref={canvasRef}
-				onMouseDown={onMouseDown}
-				width={750}
-				height={750}
-				className="border border-black rounded-md"
-			/>
 		</div>
 	);
 }
